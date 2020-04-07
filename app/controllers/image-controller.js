b8f9e32482ca5b0b98a8782599761996a6ffacaa
MatthewJOwens <mjowens@mail.com>
2020-04-06 21:35:00 -0600

    Got weather to change from F to C on click

--- app/controllers/image-controller.js
+++ app/controllers/image-controller.js
@@ -16,4 +16,7 @@ export default class ImageController {
     ImageService.getImg();
   }
 
+  getImg() {
+    ImageService.getImg()
+  }
 }
--- app/controllers/weather-controller.js
+++ app/controllers/weather-controller.js
@@ -5,17 +5,31 @@ import store from "../store.js";
 //		you may wish to check out the model and include some additional data.
 
 //TODO Complete rendering data to the screen
-function drawWeather() {
+function _drawFaren() {
   let weather = store.State.weather
   console.log("THE WEATHER MAN SAYS:", weather)
   document.getElementById("weather").innerHTML = /*html*/ `
-  <p>${weather.farenheit}&#176F in ${weather.city}</p>
+  <p onclick="app.weatherController.drawCelc()">${weather.farenheit}&#176F in ${weather.city}</p>
   `
   // < img src = "http://openweathermap.org/img/w/${weather.icon}.png" alt = "Weather Icon" >
 }
+function _drawCelc() {
+  let weather = store.State.weather
+  console.log("THE WEATHER MAN SAYS:", weather)
+  document.getElementById("weather").innerHTML = /*html*/ `
+  <p onclick="app.weatherController.drawFaren()">${weather.celcius}&#176C in ${weather.city}</p>
+  `
+}
 export default class WeatherController {
   constructor() {
-    store.subscribe("weather", drawWeather);
+    store.subscribe("weather", _drawFaren);
     WeatherService.getWeather();
+  }
+
+  drawCelc() {
+    _drawCelc()
+  }
+  drawFaren() {
+    _drawFaren()
   }
 }
